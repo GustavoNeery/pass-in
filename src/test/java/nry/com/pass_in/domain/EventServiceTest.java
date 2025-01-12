cad3f3dfa19c1ab2b9ce1751902a25a5ca0f3590
GustavoNeery <gustavoalvesneryy@hotmail.com>
2025-01-12 10:52:30 -0300

    test: update test to verify max attendee

--- src/test/java/nry/com/pass_in/domain/EventServiceTest.java
+++ src/test/java/nry/com/pass_in/domain/EventServiceTest.java
@@ -1,7 +1,13 @@
 package nry.com.pass_in.domain;
 
+import nry.com.pass_in.controllers.EventController;
 import nry.com.pass_in.domain.attendee.Attendee;
 import nry.com.pass_in.domain.event.Event;
+import nry.com.pass_in.dto.attendee.AttendeeRequestDTO;
+import nry.com.pass_in.dto.event.EventIdDTO;
+import nry.com.pass_in.dto.event.EventListResponseDTO;
+import nry.com.pass_in.dto.event.EventRequestDTO;
+import nry.com.pass_in.services.AttendeeService;
 import nry.com.pass_in.services.EventService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,31 +15,34 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class EventServiceTest {
-    private Event event;
+    private EventRequestDTO event;
     private EventService eventService;
+    private AttendeeService attendeeService;
+    private EventController eventController;
     private Attendee attendee;
-
+    private EventIdDTO eventIdDto;
+    private AttendeeRequestDTO attendeeRequestDTO;
+    private AttendeeRequestDTO attendeeRequestDTO2;
     @BeforeEach
     void setup() {
-        event = new Event();
-        event.setTitle("primeiro");
-        event.setDetails("esse é o primeiro evento");
-        event.setSlug("apenas para teste");
-        event.setMaximumAttendees(2);
+        event = new EventRequestDTO("viagem", "esse é o primeiro evento", 1);
+        eventIdDto = eventService.createEvent(event);
 
-        LocalDateTime dateTime = LocalDateTime.now();
-        attendee = new Attendee();
-        attendee.setEvent(event);
-        attendee.setName("Ivna Velez");
-        attendee.setEmail("ivnavelezteste@gmail.com");
-        attendee.setCreatedAt(dateTime);
+
+        attendeeRequestDTO = new AttendeeRequestDTO("Ivna", "ivnavelezteste@gmail.com");
+        attendeeRequestDTO2 = new AttendeeRequestDTO("Luiz", "luiznery@gmail.com");
+
     }
 
     @Test
-    @DisplayName("")
+    @DisplayName("teste inicial")
     void shouldIncrementAttendeeNumberWhenAddNewAttendee() {
-       eventService.getAllEvents();
+        eventService.registerAttendeeOnEvent(eventIdDto.eventId(), attendeeRequestDTO);
+        eventService.registerAttendeeOnEvent(eventIdDto.eventId(), attendeeRequestDTO2);
+
+       Assertions.assertEquals("Event is full", eventService.registerAttendeeOnEvent(eventIdDto.eventId(), attendeeRequestDTO2));
     }
 }
