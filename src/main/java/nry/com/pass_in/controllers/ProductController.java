4618c8370c0c9701e8a44796a58e9d2bd200e272
GustavoNeery <gustavoalvesneryy@hotmail.com>
2025-01-26 14:26:33 -0300

    feat: create find, update and delete

--- src/main/java/nry/com/pass_in/controllers/ProductController.java
+++ src/main/java/nry/com/pass_in/controllers/ProductController.java
@@ -1,5 +1,6 @@
 package nry.com.pass_in.controllers;
 
+import jakarta.transaction.Transactional;
 import nry.com.pass_in.repositories.ProductRepository;
 import org.springframework.web.bind.annotation.*;
 import nry.com.pass_in.domain.Product;
@@ -35,6 +36,34 @@ public class ProductController {
     @GetMapping("/{id}")
     public Product findById(@PathVariable String id) {
         return productRepository.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void deleteProduct(@PathVariable("id") String id) {
+        productRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable("id") String id, @RequestBody Product product) {
+        Product product1 = productRepository.findById(id);
+        if(product.getName() != null){
+            product1.setName(product.getName());
+        }
+        if(product.getDescription() != null){
+            product1.setDescription(product.getDescription());
+        }
+        if(product.getPrice() != null){
+            product1.setPrice(product.getPrice());
+        }
+        productRepository.save(product1);
+
+        return product1;
+    }
+
+    @GetMapping("/by")
+    public List<Product> findByName(@RequestParam("name") String name) {
+        return productRepository.findByName(name);
     }
 
 }
--- src/main/java/nry/com/pass_in/repositories/ProductRepository.java
+++ src/main/java/nry/com/pass_in/repositories/ProductRepository.java
@@ -3,6 +3,10 @@ package nry.com.pass_in.repositories;
 import nry.com.pass_in.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Product findById(String id);
+    void deleteById(String id);
+    List<Product> findByName(String name);
 }
